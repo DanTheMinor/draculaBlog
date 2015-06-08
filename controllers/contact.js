8e233a3e33febfbd53d8dcd1c05396cfccf920df
Cory Spitzer & Jennifer Bolanos <student@epicodus.com>
2015-06-08 13:47:08 -0700

    Reset confirmation number

--- controllers/contact.js
+++ controllers/contact.js
@@ -1,11 +1,12 @@
 Blogger.ContactController = Ember.Controller.extend({
-  confirmationNumber: Math.round(Math.random() * 1000000),
-  
+  // confirmationNumber: Math.round(Math.random() * 1000000),
+
   messageSent: false,
   actions: {
     sendMessage: function() {
       var message = prompt('Enter your message:');
       this.set('messageSent', true);
+      this.set('confirmationNumber', Math.round(Math.random() * 1000000))
     }
   }
 });
