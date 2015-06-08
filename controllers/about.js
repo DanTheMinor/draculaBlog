c1a7855e737617f6a3efdc1c2d05689f787a9a68
Cory Spitzer & Jennifer Bolanos <student@epicodus.com>
2015-06-08 12:24:11 -0700

    Hide dracula

--- controllers/about.js
+++ controllers/about.js
@@ -1,7 +1,14 @@
 Blogger.AboutController = Ember.Controller.extend({
+  isPictureShowing: false,
   actions: {
     showRealName: function() {
       alert('Dracula is...Prince Vlad III the Impaler.');
+    },
+    showPicture: function() {
+      this.set('isPictureShowing', true);
+    },
+    hidePicture: function() {
+      this.set('isPictureShowing', false);
     }
   }
 });
