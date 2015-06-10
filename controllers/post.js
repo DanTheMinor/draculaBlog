b40cfdf14718a96f354ce190670d37b9bd13ab44
Guest User <student@epicodus.com>
2015-06-10 11:25:15 -0700

    Allow posts to be added to local memory, allowed posts to be edited and deleted, allow new comments to be added

--- controllers/post.js
+++ controllers/post.js
@@ -1,12 +1,18 @@
 Blogger.PostController = Ember.ObjectController.extend({
   isEditing: false,
-  foo: "foo",
   actions: {
     edit: function() {
       this.set('isEditing', true);
     },
     save: function() {
       this.set('isEditing', false);
+      this.get('model').save();    //this line will allow the edited post to be saved and show edited version when page refreshes
+    },
+    delete: function() {
+      if (confirm('Are you sure?')) {
+        this.get('model').destroyRecord();
+        this.transitionToRoute('posts');
+      }
     }
   }
 });
